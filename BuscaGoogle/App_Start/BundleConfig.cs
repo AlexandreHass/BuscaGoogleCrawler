49f8e570ece7dff1f1ac1d168a6c05dc8330cafc
Alexandre Hass <alexandre@w3.ind.br>
2018-12-07 11:57:17 -0200

    Início

---
+++ BuscaGoogle/App_Start/BundleConfig.cs
@@ -0,0 +1,54 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace BuscaGoogle
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                                    "~/Scripts/jquery.unobtrusive*",
+                                    "~/Scripts/jquery.validate*",
+                                    "~/Scripts/methods_pt.js"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js",
+                      "~/Scripts/fastclick.js",
+                      "~/Scripts/smartresize.js",
+                      "~/Scripts/custom.js",
+                      "~/Scripts/bootstrap-datepicker.js",
+                      "~/Scripts/bootstrap-datepicker.pt-BR.js",
+                      "~/Scripts/bootstrap-table.js",
+                      "~/Scripts/bootstrap-table-pt-BR.js",
+                      "~/Scripts/bootstrap-dialog.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modalform").Include(
+                        "~/Scripts/modalform.js"));
+
+      //      var bundle1 = new ScriptBundle("~/bundles/bootstrap") { Orderer = new PassthruBundleOrderer() };
+
+
+         //   bundles.Add(bundle1);
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                                  "~/Content/bootstrap.css",
+                                  "~/Content/font-awesome.css",
+                                  "~/Content/bootstrap-datepicker.css",
+                                  "~/Content/bootstrap-table.css",
+                                  "~/Content/bootstrap-dialog.css",
+                                  "~/Content/iconesCustom.css",
+                                  "~/Content/custom.css"));
+
+            // Set EnableOptimizations to false for debugging. For more information,
+            // visit http://go.microsoft.com/fwlink/?LinkId=301862
+            BundleTable.EnableOptimizations = false;
+        }
+    }
+}
---
+++ BuscaGoogle/Controllers/ResultadoController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using HtmlAgilityPack;
+using BuscaGoogle.Models;
+
+
+namespace BuscaGoogle.Controllers
+{
+    public class ResultadoController : Controller
+    {
+
+        [ValidateInput(false)]
+        public async Task<ActionResult> Index(string termo)
+        {
+            ViewBag.Termo = termo;
+            return View(await IniciaCrawler(termo));
+        }
+
+        private async Task<List<Resultado>> IniciaCrawler(string termo)
+        {
+            var listaResultado = new List<Resultado>();
+            var url = "https://www.google.com.br/search?q=" + termo;
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            var documento = new HtmlDocument();
+            documento.LoadHtml(html);
+            var divs = documento.DocumentNode.Descendants("div")
+                .Where(node => node.GetAttributeValue("class", "").Equals("g")).ToList();
+           
+            foreach (var div in divs)
+            {
+                var resultado = new Resultado();
+                try
+                {
+                    resultado.Titulo = div.Descendants("h3").FirstOrDefault().InnerText;
+                    var link = div.Descendants("a").FirstOrDefault().ChildAttributes("href").FirstOrDefault()
+                        .Value;
+                    // remoção do amp do google para o link se tornar válido
+                    link = link.Replace("url?q=https://", "");
+                    link = link.Replace("url?q=http://", "");
+                    link = link.Substring(0, link.IndexOf("&") + 1);
+                    link = link.Replace("&", "");
+                    resultado.Link = link;
+                    listaResultado.Add(resultado);
+                }
+                catch (Exception ex)
+                {
+                    var erro = ex;
+                }
+                
+            }
+            return listaResultado;
+        }
+    }
+}
